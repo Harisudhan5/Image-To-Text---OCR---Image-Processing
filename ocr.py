446bc76b9cedfd4a21bdfa648634d36281e35fe8
Harisudhan.S <93301637+Harisudhan5@users.noreply.github.com>
2024-01-12 11:42:15 +0530

    Updated ocr.py

--- ocr.py
+++ ocr.py
@@ -19,14 +19,8 @@ for box in results.splitlines():
     cv2.rectangle(image, (x, ih-y), (w, ih-h), (0, 255, 0), 2)
     cv2.putText(image, box[0], (x, ih-h), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
 results = pytesseract.image_to_data(img_RGB)
-for id, line in enumerate(results.splitlines()):
 
-     if id != 0:
-         line = line.split()
-         if len(line) == 12:
-             x, y, w, h = int(line[6]), int(line[7]), int(line[8]), int(line[9])
-             cv2.rectangle(image, (x, y), (w+x, h+y), (0, 255, 0), 2)
-             cv2.putText(image, line[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+   
 
 cv2.imshow("Input", image)
 cv2.waitKey(0)
